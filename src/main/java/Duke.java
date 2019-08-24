34bd530cdc0812011b605174bdddebb4d4c65a7b
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2019-08-24 15:02:56 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static final String HORIZONTAL_LINE = "\t____________________________________________________________";
+    public static ArrayList<String> list = new ArrayList<String>();
 
     public static void main(String[] args) {
         startup();
@@ -17,22 +19,45 @@ public class Duke {
         output("Hello! I'm Duke\n\tWhat can I do for you?");
     }
 
+    public static void output(String input) { //inout
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("\t"+input);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void output() { //inout
+        System.out.println(HORIZONTAL_LINE);
+        for (int i = 0 ; i < list.size(); i++) {
+            int count = i+1;
+            System.out.println("\t"+count+". "+list.get(i));
+        }
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     public static void process() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (bye(input)) break;
+            else if (list(input)) continue;
             else {
-                output(input);
+                add(input);
             }
         }
         scanner.close();
     }
 
-    public static void output(String input) { //inout
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("\t"+input);
-        System.out.println(HORIZONTAL_LINE);
+    public static void add(String input) {
+        list.add(input);
+        output("added: "+input);
+    }
+
+    public static boolean list(String input) {
+        if (input.equals("list")) {
+            output();
+            return true;
+        }
+        return false;
     }
 
     public static boolean bye(String input) { //userdefined
