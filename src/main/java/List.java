37f93cb7e6d020adbe38e7ed494df6be902cfefb
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2019-08-26 16:15:11 +0800

    Level 9 Initial Commit

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class List {
     protected final String SPACE = "\t    ";
-    public ArrayList<Task> list = new ArrayList<>();
+    public ArrayList<Task> listOfTasks = new ArrayList<>();
 
     public void addList(String input) {
         Task x = new Task(input);
-        list.add(x);
+        listOfTasks.add(x);
         InOut.output("added: "+input);
     }
 
@@ -14,8 +14,8 @@ public class List {
         String[] newInput = input.split(" ");
         try {
             int i = Integer.parseInt(newInput[1]);
-            list.get(i - 1).isDone = true;
-            InOut.output("Nice! I've marked this task as done:\n" + SPACE + list.get(i - 1).getItem());
+            listOfTasks.get(i - 1).isDone = true;
+            InOut.output("Nice! I've marked this task as done:\n" + SPACE + listOfTasks.get(i - 1).getItem());
         } catch(Exception e) {
             DukeException.invalidIndex();
         }
@@ -25,10 +25,10 @@ public class List {
         String[] newInput = input.split(" ");
         try {
             int i = Integer.parseInt(newInput[1]);
-            String numberOfTasks = ((list.size()-1)!=1) ? "tasks":"task";
-            int count = list.size()-1;
-            InOut.output("Noted. I've removed this task:\n" + SPACE + list.get(i - 1).getItem()+"\n\t Now you have "+count+" "+numberOfTasks+" in the list.");
-            list.remove(i-1);
+            String numberOfTasks = ((listOfTasks.size()-1)!=1) ? "tasks":"task";
+            int count = listOfTasks.size()-1;
+            InOut.output("Noted. I've removed this task:\n" + SPACE + listOfTasks.get(i - 1).getItem()+"\n\t Now you have "+count+" "+numberOfTasks+" in the list.");
+            listOfTasks.remove(i-1);
         } catch(Exception e) {
             DukeException.invalidIndex();
         }
@@ -39,14 +39,14 @@ public class List {
         if (newInput.isEmpty()) DukeException.taskEmpty("todo");
         else {
             Task x = new ToDo(newInput);
-            list.add(x);
+            listOfTasks.add(x);
             acknowledgment();
         }
     }
 
     public void markToDo(String description, boolean isDone) {
         Task x = new ToDo(description,isDone);
-        if (!x.description.isEmpty()) list.add(x);
+        if (!x.description.isEmpty()) listOfTasks.add(x);
     }
 
     public void markDeadline(String input) {
@@ -57,7 +57,7 @@ public class List {
                 String[] finalInput = newInput.split(" /by ");
                 Task x = new Deadline(finalInput[0], finalInput[1]);
                 if (x.format) {
-                    list.add(x);
+                    listOfTasks.add(x);
                     acknowledgment();
                 } else DukeException.invalidDateAndTime();
             } catch(Exception e) {
@@ -68,7 +68,7 @@ public class List {
 
     public void markDeadline(String description, boolean isDone, String date) {
         Task x = new Deadline(description,isDone,date);
-        list.add(x);
+        listOfTasks.add(x);
     }
 
     public void markEvent(String input) {
@@ -79,7 +79,7 @@ public class List {
                 String[] finalInput = newInput.split(" /at ");
                 Task x = new Event(finalInput[0], finalInput[1]);
                 if (x.format) {
-                    list.add(x);
+                    listOfTasks.add(x);
                     acknowledgment();
                 } else DukeException.invalidDateAndTime();
             } catch(Exception e) {
@@ -90,29 +90,55 @@ public class List {
 
     public void markEvent(String description, boolean isDone, String date) {
         Task x = new Event(description,isDone,date);
-        list.add(x);
+        listOfTasks.add(x);
+    }
+
+    public void findMatchingTasks(String input) {
+        String newInput = input.substring(5);
+        if (newInput.isEmpty()) DukeException.taskEmpty("find");
+        else {
+            ArrayList<Task> matchingTasks = new ArrayList<>();
+            for (int i = 0; i < listOfTasks.size(); i++) {
+                String[] descriptions = listOfTasks.get(i).description.split(" ");
+                for (int j = 0; j < descriptions.length; j++) {
+                    if (newInput.equals(descriptions[j])) {
+                        matchingTasks.add(listOfTasks.get(i)); break;
+                    }
+                }
+            }
+            if (matchingTasks.isEmpty()) {
+                InOut.output("There are no matching tasks in the list."); return;
+            }
+            System.out.println(InOut.HORIZONTAL_LINE);
+            System.out.println("\t Here are the matching tasks in your list:");
+            for (int i = 0 ; i < matchingTasks.size(); i++) {
+                int count = i+1;
+                System.out.println("\t "+count+". "+matchingTasks.get(i).getItem());
+            }
+            System.out.println(InOut.HORIZONTAL_LINE);
+        }
     }
 
     public void printList() {
-        if (list.isEmpty()) {
+        if (listOfTasks.isEmpty()) {
             InOut.output("There are no tasks in the list."); return;
         }
         System.out.println(InOut.HORIZONTAL_LINE);
         System.out.println("\t Here are the tasks in your list:");
-        for (int i = 0 ; i < list.size(); i++) {
+        for (int i = 0 ; i < listOfTasks.size(); i++) {
             int count = i+1;
-            System.out.println("\t "+count+". "+list.get(i).getItem());
+            System.out.println("\t "+count+". "+listOfTasks.get(i).getItem());
         }
         System.out.println(InOut.HORIZONTAL_LINE);
     }
 
     public String writeListToFile() {
         String output = "";
-        for (int i = 0 ; i < list.size(); i++) {
-            String task = list.get(i).getItem().substring(1,2);
-            output += (task+";"+list.get(i).isDone+";"+list.get(i).description);
+        for (int i = 0 ; i < listOfTasks.size(); i++) {
+            String task = listOfTasks.get(i).getItem().substring(1,2);
+            output += (task+";"+listOfTasks.get(i).isDone+";"+listOfTasks.get(i).description);
             if (task.equals("D") || task.equals("E")) {
-                String[] temp = list.get(i).getItem().split(": ");
+                String[] temp = listOfTasks.get(i).getItem().split(": ");
                 output += ";"+temp[1].substring(0,temp[1].length()-1);
             }
             output+="\n";
@@ -121,7 +147,7 @@ public class List {
     }
 
     protected void acknowledgment() {
-        String numberOfTasks = (list.size()!=1) ? "tasks":"task";
-        InOut.output("Got it. I've added this task:\n"+SPACE+list.get(list.size()-1).getItem()+"\n\t Now you have "+list.size()+" "+numberOfTasks+" in the list.");
+        String numberOfTasks = (listOfTasks.size()!=1) ? "tasks":"task";
+        InOut.output("Got it. I've added this task:\n"+SPACE+listOfTasks.get(listOfTasks.size()-1).getItem()+"\n\t Now you have "+listOfTasks.size()+" "+numberOfTasks+" in the list.");
     }
 }
