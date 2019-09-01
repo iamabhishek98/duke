25915a0081f13abb2127dc3211b6b5029666e117
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2019-09-01 13:56:41 +0800

    changed runtest script to move data file

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -2,6 +2,7 @@
 
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
+if not exist \src\main\data mkdir \src\main\data
 
 REM delete output from previous run
 del ACTUAL.TXT
@@ -14,8 +15,21 @@ IF ERRORLEVEL 1 (
 )
 REM no error here, errorlevel == 0
 
+REM move data file to text-ui-test directory
+cd ..\src\main\data
+move duke.txt  ..\..\..\text-ui-test\src\main\data\
+
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+cd ..\..\..\text-ui-test
 java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+
+REM move data file back to original directory
+cd .\src\main\data
+move duke.txt ..\..\..\..\src\main\data\
+
+REM to delete temp src directory created
+REM cd ..\..\..
+REM rd /s/q "src"
